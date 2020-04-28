4ef32bd803c97ef92e7978b2f7d032745d9ff21a
Yoda92 <andersfisker1007@outlook.com>
2020-04-28 13:06:15 +0200

    Added NSub

---
+++ Microwave.Test.Integrations/CookControllerDriver.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Interfaces;
+using MicrowaveOvenClasses.Controllers;
+using NUnit.Framework;
+using NSubstitute;
+
+namespace Microwave.Test.Integrations
+{
+    [TestFixture]
+    class CookControllerDriver
+    {
+
+        private Display display;
+        private Output output;
+        private PowerTube powertube;
+        private Timer timer;
+        private CookController cookcontroller;
+        private IUserInterface userinteface;
+
+        [SetUp]
+        public void Setup()
+        {
+            output = new Output();
+            display = new Display(output);
+            powertube = new PowerTube(output);
+            timer = new Timer();
+            userinteface = Substitute.For<IUserInterface>();
+            cookcontroller = new CookController(timer, display, powertube, userinteface);
+        }
+
+        [Test]
+        public void StartCooking_ValidParameters_TimerStarted()
+        {
+            uut.StartCooking(50, 60);
+
+            timer.Received().Start(60);
+        }
+
+        [Test]
+        public void StartCooking_ValidParameters_PowerTubeStarted()
+        {
+            uut.StartCooking(50, 60);
+
+            powerTube.Received().TurnOn(50);
+        }
+
+        [Test]
+        public void Cooking_TimerTick_DisplayCalled()
+        {
+            uut.StartCooking(50, 60);
+
+            timer.TimeRemaining.Returns(115);
+            timer.TimerTick += Raise.EventWith(this, EventArgs.Empty);
+
+            display.Received().ShowTime(1, 55);
+        }
+
+        [Test]
+        public void Cooking_TimerExpired_PowerTubeOff()
+        {
+            uut.StartCooking(50, 60);
+
+            timer.Expired += Raise.EventWith(this, EventArgs.Empty);
+
+            powerTube.Received().TurnOff();
+        }
+
+        [Test]
+        public void Cooking_TimerExpired_UICalled()
+        {
+            uut.StartCooking(50, 60);
+
+            timer.Expired += Raise.EventWith(this, EventArgs.Empty);
+
+            ui.Received().CookingIsDone();
+        }
+
+        [Test]
+        public void Cooking_Stop_PowerTubeOff()
+        {
+            uut.StartCooking(50, 60);
+            uut.Stop();
+
+            powerTube.Received().TurnOff();
+        }
+
+    }
+
+}
