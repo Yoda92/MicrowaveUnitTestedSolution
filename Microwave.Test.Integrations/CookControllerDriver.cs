00b1e6834b56396144ded4ded25051bb83c80029
Yoda92 <andersfisker1007@outlook.com>
2020-04-28 15:04:19 +0200

    Implemented CookingControllerDriver

--- Microwave.Test.Integrations/CookControllerDriver.cs
+++ Microwave.Test.Integrations/CookControllerDriver.cs
@@ -23,7 +23,7 @@ namespace Microwave.Test.Integrations
         private PowerTube _powertube;
         private Timer _timer;
         private CookController _cookcontroller;
-        private IUserInterface _userinteface;
+        private IUserInterface _userinterface;
 
         [SetUp]
         public void Setup()
@@ -32,11 +32,13 @@ namespace Microwave.Test.Integrations
             _display = new Display(_output);
             _powertube = new PowerTube(_output);
             _timer = new Timer();
-            _userinteface = Substitute.For<IUserInterface>();
-            _cookcontroller = new CookController(_timer, _display, _powertube, _userinteface);
+            _userinterface = Substitute.For<IUserInterface>();
+            _cookcontroller = new CookController(_timer, _display, _powertube, _userinterface);
         }
 
         [TestCase(50, 2)]
+        [TestCase(99, 5)]
+        [TestCase(20, 8)]
         public void StartCookingShouldTurnOnPowerTubeAndStartATimer(int power, int seconds) {
             _cookcontroller.StartCooking(power, seconds);
             _output.Received().OutputLine(Arg.Is<string>(str => str == $"PowerTube works with {power}"));
@@ -48,6 +50,38 @@ namespace Microwave.Test.Integrations
             sec = (seconds - 1) % 60;
 
             _output.Received().OutputLine(Arg.Is<string>(str => str == $"Display shows: {min:D2}:{sec:D2}"));
+        }
+
+        [Test]
+        public void StartCookingShouldTurnOffPowerAndShowCookingIsDoneWhenTimerExpires() {
+            int seconds = 2;
+            int power = 40;
+            _cookcontroller.StartCooking(power, seconds);
+            
+            _output.Received().ClearReceivedCalls();
+            Thread.Sleep(3000);
+            _output.Received().OutputLine(Arg.Is<string>(str => str == "PowerTube turned off"));
+            _userinterface.Received().CookingIsDone();       
+        }
+
+        [Test]
+        public void StopCookingDoesNothingIfNotAlreadyCooking() {
+            _cookcontroller.Stop();
+
+            _output.DidNotReceive().OutputLine(Arg.Is<string>(str => str == "PowerTube turned off"));
+        }
+
+        [Test]
+        public void StopCookingTurnsOffPowerTubeAndTimer() {
+            _cookcontroller.StartCooking(50, 3);
+            _output.ClearReceivedCalls();
+
+            _cookcontroller.Stop();
+
+            _output.Received().OutputLine(Arg.Is<string>(str => str == "PowerTube turned off"));
+            _output.ClearReceivedCalls();
+            Thread.Sleep(1500);
+            _output.DidNotReceive().OutputLine(Arg.Any<string>());
         }
 
     }
